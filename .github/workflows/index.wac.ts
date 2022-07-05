66e5abdc369bbd6b7128543a102bdbbbdb4c1765
Adrian Smijulj <adrian1358@gmail.com>
2022-07-05 20:05:06 +0200

    fix: update bin prop

--- .github/workflows/index.wac.ts
+++ .github/workflows/index.wac.ts
@@ -35,7 +35,7 @@ export const push = createWorkflow({
                 {
                     name: "Release",
                     uses: "cycjimmy/semantic-release-action@v3",
-                    with: { "working-directory": "./dist" },
+                    with: { "working_directory": "./dist" },
                     env: {
                         GITHUB_TOKEN: "${{ secrets.GITHUB_TOKEN }}",
                         NPM_TOKEN: "${{ secrets.NPM_TOKEN }}"
